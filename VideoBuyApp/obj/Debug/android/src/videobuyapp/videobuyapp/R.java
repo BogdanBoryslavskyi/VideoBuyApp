508bc7b23a0f9670ba2eb571ed2f83580bae75a6
GeniusAtamans <programer.cplasplas@gmail.com>
2014-09-17 17:18:32 +0300

    Add new bg

--- VideoBuyApp/obj/Debug/android/src/videobuyapp/videobuyapp/R.java
+++ VideoBuyApp/obj/Debug/android/src/videobuyapp/videobuyapp/R.java
@@ -11,13 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int blue_bg=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int splash=0x7f020002;
-        public static final int video_thumbnail_default1=0x7f020003;
-        public static final int video_thumbnail_default2=0x7f020004;
-        public static final int wood_bg1=0x7f020005;
-        public static final int wood_bg2=0x7f020006;
+        public static final int black_bg1=0x7f020000;
+        public static final int blue_bg=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int splash=0x7f020003;
+        public static final int video_thumbnail_default1=0x7f020004;
+        public static final int video_thumbnail_default2=0x7f020005;
+        public static final int wood_bg1=0x7f020006;
+        public static final int wood_bg2=0x7f020007;
     }
     public static final class id {
         public static final int MassageField=0x7f06000a;
